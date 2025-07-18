e11d48944826285b796e61db58474c30b0753f47
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-07-18 17:08:44 +0900

    11_2 성능 최적화 실습

---
+++ src/ch10_app/components/TodoInsert.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import React, { useCallback, useState } from 'react';
+import { MdAdd } from 'react-icons/md';
+import '@/ch10_app/css/TodoInsert.scss';
+
+const TodeInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={onChange}
+        name="todo"
+        placeholder="할 일을 입력하세요"
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+TodeInsert.propTypes = {
+  onInsert: PropTypes.func.isRequired,
+};
+
+export default TodeInsert;
---
+++ src/ch10_app/components/TodoListItem.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import '@/ch10_app/css/TodoListItem.scss';
+
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
+
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+TodoListItem.propTypes = {
+  todo: PropTypes.object,
+  onRemove: PropTypes.func,
+  onToggle: PropTypes.func,
+};
+
+export default TodoListItem;
