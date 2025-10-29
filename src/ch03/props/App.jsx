1c7ec7b497f54f32a8459585a8293fad4e5fc438
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2025-10-29 12:09:16 +0900

    feat) 문법 오류 수정

--- src/ch03/props/App.jsx
+++ src/ch03/props/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './ch03/props/MyComponent';
 
 const App = () => {
-  return <MyComponent name="React"/>;
+  return <MyComponent name="React" />;
 };
 
 export default App;
